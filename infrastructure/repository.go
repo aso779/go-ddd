6d7e54b2b8537f1e6b707727fc19dad0cf9b5253
aso779 <asopochta@gmail.com>
2022-03-22 05:57:16 +0200

    add tx type to repository interface

--- infrastructure/repository.go
+++ infrastructure/repository.go
@@ -6,61 +6,61 @@ import (
 	"github.com/aso779/go-ddd/domain/usecase/metadata"
 )
 
-type CrudRepository[T metadata.Entity] interface {
+type CrudRepository[E metadata.Entity, T any] interface {
 	FindOne(
 		ctx context.Context,
-		tx TransactionManager,
+		tx TransactionManager[T],
 		fields []string,
 		spec dataset.Specifier,
-	) (*T, error)
+	) (*E, error)
 
 	FindOneById(
 		ctx context.Context,
-		tx TransactionManager,
+		tx TransactionManager[T],
 		fields []string,
 		id metadata.PrimaryKey,
-	) (*T, error)
+	) (*E, error)
 
 	FindAll(
 		ctx context.Context,
-		tx TransactionManager,
+		tx TransactionManager[T],
 		fields []string,
 		spec dataset.Specifier,
 		page dataset.Pager,
 		sort dataset.Sorter,
-	) (*[]T, error)
+	) (*[]E, error)
 
 	FindAllByIds(
 		ctx context.Context,
-		tx TransactionManager,
+		tx TransactionManager[T],
 		fields []string,
 		ids []metadata.PrimaryKey,
-	) (*[]T, error)
+	) (*[]E, error)
 
 	Count(
 		ctx context.Context,
-		tx TransactionManager,
+		tx TransactionManager[T],
 		spec dataset.Specifier,
 	) (int, error)
 
 	Create(
 		ctx context.Context,
-		tx TransactionManager,
-		ent *T,
+		tx TransactionManager[T],
+		ent *E,
 		fields []string,
-	) (*T, error)
+	) (*E, error)
 
 	Update(
 		ctx context.Context,
-		tx TransactionManager,
-		ent *T,
+		tx TransactionManager[T],
+		ent *E,
 		fields []string,
 		ftu []string,
-	) (*T, error)
+	) (*E, error)
 
 	Delete(
 		ctx context.Context,
-		tx TransactionManager,
+		tx TransactionManager[T],
 		spec dataset.Specifier,
 	) (int, error)
 }
