9e0abb9e31a9fecc63e15670d269525764e59c12
aso779 <asopochta@gmail.com>
2022-03-22 23:55:53 +0200

    up pager interface

--- domain/usecase/dataset/pager.go
+++ domain/usecase/dataset/pager.go
@@ -2,12 +2,12 @@ package dataset
 
 //Pager limit offset pager interface
 type Pager interface {
-	//Size page size
-	Size() int
-	//Number page number
-	Number() int
-	//Offset page offset
-	Offset() int
+	//GetSize page size
+	GetSize() int
+	//GetNumber page number
+	GetNumber() int
+	//GetOffset page offset
+	GetOffset() int
 	//IsEmpty is empty flag
 	IsEmpty() bool
 }
