5205adcbd993b410f793477a50893e4c4b75e802
aso779 <asopochta@gmail.com>
2022-04-21 15:27:05 +0300

    add composite_in dataspec

---
+++ infrastructure/dataspec/composite_in.go
@@ -0,0 +1,46 @@
+package dataspec
+
+import (
+	"fmt"
+	"github.com/aso779/go-ddd/domain/usecase/dataset"
+	"github.com/aso779/go-ddd/domain/usecase/metadata"
+	"strings"
+)
+
+type CompositeInSpecification struct {
+	fields []Field
+	values []interface{}
+}
+
+func CompositeNewIn(fields []string, values []interface{}) dataset.Specifier {
+	var fieldSet []Field
+	for _, v := range fields {
+		fieldSet = append(fieldSet, NewField(v))
+	}
+
+	return &CompositeInSpecification{
+		fields: fieldSet,
+		values: values,
+	}
+}
+
+func (r *CompositeInSpecification) Joins(meta metadata.Meta) []string {
+	return []string{}
+}
+
+func (r CompositeInSpecification) Query(meta metadata.Meta) string {
+	var columnNames []string
+	for _, v := range r.fields {
+		columnNames = append(columnNames, v.ColumnName(meta))
+	}
+
+	return fmt.Sprintf("(%s) IN (?)", strings.Join(columnNames, ","))
+}
+
+func (r CompositeInSpecification) Values() []interface{} {
+	return []interface{}{r.values}
+}
+
+func (r *CompositeInSpecification) IsEmpty() bool {
+	return len(r.values) == 0
+}
